0401e46741922f42e0245ee4fd0cf96f5c0af720
Chris Jacobs <cjpdx.dev@gmail.com>
2020-07-19 13:24:51 +0000

    started adding test comments

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,13 @@ from selenium import webdriver
 
 browser = webdriver.Firefox()
 
+
+# Test that homepage exists 
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Test that the page title and header are accurate
+assert 'Django' in browser.title 
+
+# Test that a to-do item can be entered immediately
+
+
