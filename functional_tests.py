ce2d8a589598119f42f0e5f99db8611b0c6770d1
Chris <cjpdx.dev@gmail.com>
2020-07-29 18:17:49 -0400

    created and passed first unit test of url mapping

--- functional_tests.py
+++ functional_tests.py
@@ -6,8 +6,9 @@ class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+	# uncomment when we get tired of opening firefox windows
+	# def tearDown(self):
+		# self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
