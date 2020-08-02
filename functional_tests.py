dfbae6bae09ed333559c696972079f6cae05e759
Chris <cjpdx.dev@gmail.com>
2020-08-01 23:01:12 -0400

    Functional test now checks we can input a to-do time

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,21 +22,36 @@ class NewVisitorTest(unittest.TestCase):
 		# Test 2
 		# Test that the page title and header are accurate
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# Test 3
 		# Test that a to-do item can be entered immediately
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		# Test 4
-		# Unit tests for to-do item
+		# Test an entry into the text box
+		inputbox.send_keys('1. Test entry.')
 
 		# Test 5
 		# After entering to-do item, test whether the page has
 		# has updated successfully and is now displaying the to-do item.
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_elements_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Test entry.' for row in rows)
+		)
 
 		# Test 6
 		# Repeat Test 5 on a second to-do item
-
+		self.fail('Finish the test (Test 6)')
 		# Test 7
 		# Test that the app remembers the list by generating a unique URL
 
--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,6 @@ from django.urls import resolve
 
 from lists.views import home_page
 
-
 # Create your tests here.
 
 # class SmokeTest(TestCase):
